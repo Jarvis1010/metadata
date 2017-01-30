f3a45042982c5c9dc89e44478aa6884a33291653
Justin Travis Mair <travis.mair@gmail.com>
2017-01-30 16:38:55 +0000

    First Commit

---
+++ server.js
@@ -0,0 +1,42 @@
+var express=require('express');
+var app=express();
+var path = require('path');
+
+var port = process.env.PORT||8080;
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/:Date',function(req,res){
+   var response={ "unix": null, "natural": null };
+  
+   if(isNaN(req.params.Date)){
+      var d = new Date(req.params.Date);
+     
+      if(Date.parse(req.params.Date)){
+        response.unix=d.getTime() / 1000;
+        response.natural=req.params.Date;
+      }
+   }else{
+      
+      var d = new Date(req.params.Date*1000);
+      if(d.getTime()!==null){
+        var dateString=naturalDateString(d);
+        response.natural=dateString;
+        response.unix=req.params.Date;
+      }
+   }
+    res.send(response);
+});
+
+function naturalDateString(d){
+    var months={0:'January',1:'February',2:'March',3:'April',4:'May',5:'June',6:'July',7:'August',8:'September',9:'October',10:'November',11:'December'};
+    return months[d.getMonth()]+" "+d.getDate()+", "+d.getFullYear();
+}
+
+app.listen(port,function(){
+    console.log('serverstarted');
+});
