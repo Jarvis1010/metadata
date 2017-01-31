21a99c3de385a12305acef8176f7813f946d5dc9
Justin Travis Mair <travis.mair@gmail.com>
2017-01-31 00:46:29 +0000

    Minor change to final

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', function(req, res) {
 
 
 app.post('/get-file-size',upload.single('file'), function(req, res) {
-    console.log(req);
+    
   var size = req.file.size;
   var response={ "Filesize": size };
   
